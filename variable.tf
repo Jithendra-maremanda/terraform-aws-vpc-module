b7cbc1a975b9b23eb224ca72cd1e1e01e261e2db
Jithendra-maremanda <jithendramaremanda@gmail.com>
2025-07-04 22:19:41 +0530

    code updated

--- variable.tf
+++ variable.tf
@@ -10,8 +10,86 @@ variable "environment" {
     type = string
   
 }
+
+#Tags for VPC and IGW
+
+variable "vpc_tags" {
+  type = map(string)
+  default = {}
+  
+}
+variable "igw_tags" {
+  type = map(string)
+  default = {}
+  
+}
+variable "eip_tags" {
+  type = map(string)
+  default = {}
+  
+}
+variable "nat_gateway_tags" {
+  type = map(string)
+  default = {}
+  
+}
+
+#subnet CIDRs
 variable "public_subnet_cidrs" {
   type = list(string)
   
+}
+variable "private_subnet_cidrs" {
+  type = list(string)
+  
+}
+variable "database_subnet_cidrs" {
+  type = list(string)
+  
+}
+
+
+# subnet tags
+variable "public_subnet_tags" {
+  type = map(string)
+  default = {}
+  
+}
+
+variable "private_subnet_tags" {
+  type = map(string)
+  default = {}
+  
+}
+
+variable "database_subnet_tags" {
+  type = map(string)
+  default = {}
+  
+}
+variable "vpc_peering_tags" {
+  type = map(string)
+  default = {}
+  
+}
+
+
+# Tags for route tables
+variable "public_route_table_tags" {
+  type = map(string)
+  default = {}
+}
+
+variable "private_route_table_tags" {
+  type = map(string)
+  default = {}
+}
+variable "database_route_table_tags" {
+  type = map(string)
+  default = {}
+}
+variable "is_preering_required" {
+  type = bool
+  default = false
   
 }
